af08f12f085d6ad729efba817907fc1be1891465
Ruben <rdg53@drexel.edu>
2018-11-07 14:58:05 -0500

    Ability to make more walls spawn
    You can spawn more than one wall from picking through the list and pressing start

--- WallE/Assets/Scripts/WallMover.cs
+++ WallE/Assets/Scripts/WallMover.cs
@@ -1,66 +1,89 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WallMover : MonoBehaviour {
 
     Vector3 startPosition;
     GameObject wallPositionParent;
     GameObject wall;
-
+    List<GameObject> poseList = new List<GameObject>();
+     
     bool wallSpawned = false;
 
     public Canvas posePicker;
 
 	// Use this for initialization
+    //find start position for walls
 	void Start () {
         startPosition = new Vector3(0, 25, 500);
-	}
 
-
-    public void SpawnWall(GameObject cubeName)
-    {
-        this.HideShowPicker();
         wallPositionParent = GameObject.Find("wallPosition");
-        wall = Instantiate(cubeName, wallPositionParent.transform.position, new Quaternion(0, 0, 0, 0));
-        //wall.transform.SetPositionAndRotation(new Vector3(0, 0, 0),new Quaternion(0,0,0,0));
-        wall.transform.parent = wallPositionParent.transform;
-        wall.transform.localScale = new Vector3(0.01f,0.01f,0.01f);
+    }
 
-        //wallSpawned = true;
-        StartCoroutine(MoveOverSeconds(wall,new Vector3(0,0,0), 5f));
-
-        /*if (wallSpawned)
+    //add or remove pose to list when button is clicked
+    public void AddRemovePose(GameObject cubeName)
+    {
+        if (poseList.Contains(cubeName))
         {
-            Destroy(wall);
-            wallSpawned = false;
-
-        }*/
+            poseList.Remove(cubeName);
+        }
+        else
+        {
+            poseList.Add(cubeName);
+            Debug.Log(cubeName.ToString());
+        }
 
     }
 
+    public void ToggleButton(Button button)
+    {
+        if (button.colors.highlightedColor.r != 1.0f)
+        {
+            
+        }
+
+        Debug.Log(button.colors.highlightedColor.ToString());
+    }
+
+    //spawn the entire list of walls
+    public void SpawnWallList()
+    {     
+        this.HideShowPicker();
+        StartCoroutine(MoveOverSeconds(new Vector3(0, 0, 0), 5f));
+    }
+
+    //hide/show the picker canvas
     void HideShowPicker()
     {
         posePicker.enabled = !posePicker.isActiveAndEnabled;
     }
 
-
-    public IEnumerator MoveOverSeconds(GameObject objectToMove, Vector3 end, float seconds)
+    //spawn, parent, and move the walls from start to end in 5 seconds
+    public IEnumerator MoveOverSeconds(Vector3 end, float seconds)
     {
-        float elapsedTime = 0;
-        Vector3 startingPos = objectToMove.transform.position;
-        while (elapsedTime < seconds)
+        foreach (GameObject cube in poseList)
         {
-            objectToMove.transform.position = Vector3.Lerp(startingPos, end, (elapsedTime / seconds));
-            elapsedTime += Time.deltaTime;
-            yield return new WaitForEndOfFrame();
+            wall = Instantiate(cube, wallPositionParent.transform.position, new Quaternion(0, 0, 0, 0));
+            wall.transform.parent = wallPositionParent.transform;
+            wall.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+
+            float elapsedTime = 0;
+            Vector3 startingPos = wall.transform.position;
+
+            while (elapsedTime < seconds)
+            {
+                wall.transform.position = Vector3.Lerp(startingPos, end, (elapsedTime / seconds));
+                elapsedTime += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+            wall.transform.position = end;
+
+            Destroy(wall);
         }
-        objectToMove.transform.position = end;
-
-        Destroy(wall);
         this.HideShowPicker();
-
-        Debug.Log("Over");
+        poseList.Clear();
     }
 
     // Update is called once per frame
