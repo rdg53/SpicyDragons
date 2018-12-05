afda8698afcbaa8b777699c36c05642cb12a2a10
Ruben <rdg53@drexel.edu>
2018-12-05 11:55:37 -0500

    Feedback functionality
    leaving the head of instructor changes colors for feedback

---
+++ WallE/Assets/Scripts/ColliderCheck.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderCheck : MonoBehaviour {
+
+    public SphereCollider avatarCollider;
+    public SphereCollider animationCollider;
+    public GameObject fireflyGeo;
+    public Material red;
+    public Material green;
+    
+    
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerExit(Collider collider)
+    {
+        //Debug.LogWarning("yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeees");
+       // Debug.LogWarning(collider.gameObject);
+        Material[] mats = new Material[] { red };
+        fireflyGeo.GetComponent<SkinnedMeshRenderer>().materials = mats;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("askdnaiushduiahuisdhuiahsduihauids");
+        Material[] mats = new Material[] { green };
+        fireflyGeo.GetComponent<SkinnedMeshRenderer>().materials = mats;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
